ab5c995b693d94d09251b061833b48e9827143e1
tasspower <nattapatjuambo@gmail.com>
2025-01-21 11:41:35 +0700

    Add files via upload

---
+++ item03.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp3
+{
+    internal class item03
+    {
+        public string name;
+        public int price;
+        public int quantity;
+        public bool isCheck;
+    }
+}
